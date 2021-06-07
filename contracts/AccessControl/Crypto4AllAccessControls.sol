d5bf0a8d22e09c723171a818e3f26dc4e0a28ad3
lifeisgooddev <lifeisgood91028@gmail.com>
2021-06-07 10:27:08 -0400

    Add NFT & AccessControls contract

---
+++ contracts/AccessControl/Crypto4AllAccessControls.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+/**
+ * @notice Access Controls contract for the Non Figgybles Platform
+ */
+contract Crypto4AllAccessControls is AccessControl {
+    /// @notice Role definitions
+    bytes32 public constant MINTER_ROLE = 
+    0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6; // keccak256("MINTER_ROLE")
+
+    /// @notice Events for adding and removing various roles
+    event AdminRoleGranted(
+        address indexed beneficiary,
+        address indexed caller
+    );
+
+    event AdminRoleRemoved(
+        address indexed beneficiary,
+        address indexed caller
+    );
+
+    event MinterRoleGranted(
+        address indexed beneficiary,
+        address indexed caller
+    );
+
+    event MinterRoleRemoved(
+        address indexed beneficiary,
+        address indexed caller
+    );
+
+    /**
+     * @notice The deployer is automatically given the admin role which will allow them to then grant roles to other addresses
+     */
+    constructor() public {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+    /////////////
+    // Lookups //
+    /////////////
+
+    /**
+     * @notice Used to check whether an address has the admin role
+     * @param _address EOA or contract being checked
+     * @return bool True if the account has the role or false if it does not
+     */
+    function hasAdminRole(address _address) external view returns (bool) {
+        return hasRole(DEFAULT_ADMIN_ROLE, _address);
+    }
+
+    /**
+     * @notice Used to check whether an address has the minter role
+     * @param _address EOA or contract being checked
+     * @return bool True if the account has the role or false if it does not
+     */
+    function hasMinterRole(address _address) external view returns (bool) {
+        return hasRole(MINTER_ROLE, _address);
+    }
+
+
+    ///////////////
+    // Modifiers //
+    ///////////////
+
+    /**
+     * @notice Grants the admin role to an address
+     * @dev The sender must have the admin role
+     * @param _address EOA or contract receiving the new role
+     */
+    function addAdminRole(address _address) external {
+        grantRole(DEFAULT_ADMIN_ROLE, _address);
+        emit AdminRoleGranted(_address, _msgSender());
+    }
+
+    /**
+     * @notice Removes the admin role from an address
+     * @dev The sender must have the admin role
+     * @param _address EOA or contract affected
+     */
+    function removeAdminRole(address _address) external {
+        revokeRole(DEFAULT_ADMIN_ROLE, _address);
+        emit AdminRoleRemoved(_address, _msgSender());
+    }
+
+    /**
+     * @notice Grants the minter role to an address
+     * @dev The sender must have the admin role
+     * @param _address EOA or contract receiving the new role
+     */
+    function addMinterRole(address _address) external {
+        grantRole(MINTER_ROLE, _address);
+        emit MinterRoleGranted(_address, _msgSender());
+    }
+
+    /**
+     * @notice Removes the minter role from an address
+     * @dev The sender must have the admin role
+     * @param _address EOA or contract affected
+     */
+    function removeMinterRole(address _address) external {
+        revokeRole(MINTER_ROLE, _address);
+        emit MinterRoleRemoved(_address, _msgSender());
+    }
+
+}
---
+++ contracts/NFT/Crypto4AllNFT.sol
@@ -0,0 +1,148 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "../AccessControl/Crypto4AllAccessControls.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/EnumerableSet.sol";
+
+/**
+ * @title Crypto4All  NFT
+ * @dev Issues ERC-721 tokens 
+ */
+contract Crypto4AllNFT is ERC721("Crypto4All NFT", "CFAN") {
+
+    // @notice event emitted upon construction of this contract, used to bootstrap external indexers
+    event Crypto4AllNFTContractDeployed();
+
+    // @notice event emitted when token URI is updated
+    event Crypto4AllTokenUriUpdate(
+        uint256 indexed _tokenId,
+        string _tokenUri
+    );
+
+    /// @dev Required to govern who can call certain functions
+    Crypto4AllAccessControls public accessControls;
+
+    /// @dev current max tokenId
+    uint256 public tokenIdPointer;
+
+    /// @dev TokenID -> Post Creator address
+    mapping(uint256 => address) public postCreators;
+
+    /**
+     @notice Constructor
+     @param _accessControls Address of the Crypto4AllNFT access control contract
+     */
+    constructor(Crypto4AllAccessControls _accessControls) public {
+        accessControls = _accessControls;
+        emit Crypto4AllNFTContractDeployed();
+    }
+
+    /**
+     @notice Mints a Crypto4AllNFT AND when minting to a contract checks if the beneficiary is a 721 compatible
+     @dev Only senders with either the admin or mintor role can invoke this method
+     @param _beneficiary Recipient of the NFT
+     @param _tokenUri URI for the token being minted
+     @param _postCreator NFT Creator - will be required for issuing royalties from secondary sales
+     @return uint256 The token ID of the token that was minted
+     */
+    function mint(address _beneficiary, string calldata _tokenUri, address _postCreator) external returns (uint256) {
+        require(
+            accessControls.hasAdminRole(_msgSender()) || accessControls.hasMinterRole(_msgSender()),
+            "Crypto4AllNFT.mint: Sender must have the admin or minter role"
+        );
+        // Valid args
+        _assertMintingParamsValid(_tokenUri, _postCreator);
+
+        tokenIdPointer = tokenIdPointer.add(1);
+        uint256 tokenId = tokenIdPointer;
+
+        // Mint token and set token URI
+        _safeMint(_beneficiary, tokenId);
+        _setTokenURI(tokenId, _tokenUri);
+
+        postCreators[tokenId] = _postCreator;
+
+        return tokenId;
+    }
+
+    /**
+     @notice Burns a Crypto4AllNFT
+     @dev Only the owner or an approved sender can call this method
+     @param _tokenId the token ID to burn
+     */
+    function burn(uint256 _tokenId) external {
+        address operator = _msgSender();
+        require(
+            ownerOf(_tokenId) == operator || isApproved(_tokenId, operator),
+            "Crypto4AllNFT.burn: Only garment owner or approved"
+        );
+        // Destroy token mappings
+        _burn(_tokenId);
+
+        delete postCreators[_tokenId];
+    }
+
+
+
+    //////////
+    // Admin /
+    //////////
+
+    /**
+     @notice Updates the token URI of a given token
+     @dev Only admin
+     @param _tokenId The ID of the token being updated
+     @param _tokenUri The new URI
+     */
+    function setTokenURI(uint256 _tokenId, string calldata _tokenUri) external {
+        require(
+            accessControls.hasAdminRole(_msgSender()),"Crypto4AllNFT.setTokenURI: Sender must have the admin role"
+        );
+        _setTokenURI(_tokenId, _tokenUri);
+        emit Crypto4AllTokenUriUpdate(_tokenId, _tokenUri);
+    }
+
+    /**
+     @notice Method for updating the access controls contract used by the NFT
+     @dev Only admin
+     @param _accessControls Address of the new access controls contract
+     */
+    function updateAccessControls(Crypto4AllAccessControls _accessControls) external {
+        require(accessControls.hasAdminRole(_msgSender()), "Crypto4AllNFT.updateAccessControls: Sender must be admin");
+        accessControls = _accessControls;
+    }
+    /////////////////
+    // View Methods /
+    /////////////////
+
+    /**
+     @notice View method for checking whether a token has been minted
+     @param _tokenId ID of the token being checked
+     */
+    function exists(uint256 _tokenId) external view returns (bool) {
+        return _exists(_tokenId);
+    }
+
+    /**
+     * @dev checks the given token ID is approved either for all or the single token ID
+     */
+    function isApproved(uint256 _tokenId, address _operator) public view returns (bool) {
+        return isApprovedForAll(ownerOf(_tokenId), _operator) || getApproved(_tokenId) == _operator;
+    }
+
+    /////////////////////////
+    // Internal and Private /
+    /////////////////////////
+
+    /**
+     @notice Checks that the URI is not empty and the post creator is a real address
+     @param _tokenUri URI supplied on minting
+     @param _postCreator Address supplied on minting
+     */
+    function _assertMintingParamsValid(string calldata _tokenUri, address _postCreator) pure internal {
+        require(bytes(_tokenUri).length > 0, "Crypto4AllNFT._assertMintingParamsValid: Token URI is empty");
+        require(_postCreator != address(0), "Crypto4AllNFT._assertMintingParamsValid: creator is zero address");
+    }
+}
