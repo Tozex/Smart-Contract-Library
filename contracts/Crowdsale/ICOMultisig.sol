fc80263aa3dabdbfbf530ab1c05ac15398f0e738
Clement Rodallec <clem.2002.cr@gmail.com>
2024-06-06 19:57:46 +0200

    fix: Update import path for IMultiSigWallet in ICOMultisig.sol

--- contracts/Crowdsale/ICOMultisig.sol
+++ contracts/Crowdsale/ICOMultisig.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../contracts/IMultiSigWallet.sol";
+import "../MultiSigWallet/IMultiSigWallet.sol";
 
 // SPDX-License-Identifier: GPL-3.0
 
