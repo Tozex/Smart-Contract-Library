2e781e9608118433b9f15fa7bdf5112ff370f9d3
dragonheaven <dragonhub99@gmail.com>
2024-06-23 21:28:37 +0200

    fix the feedback

--- contracts/Factory/VaultProxyBeaconFactory.sol
+++ contracts/Factory/VaultProxyBeaconFactory.sol
@@ -7,20 +7,22 @@ import "../MultiSigWallet/MultiSigWalletAPI.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
  contract VaultProxyBeaconFactory is OwnableUpgradeable{
     address public beacon;
-
     address[] public beacons;
+    address public owner;
     
-    event VaultCreated(address proxyAddress, address owner, address[] signers, uint required, string indentifier);
+    event VaultCreated(address proxyAddress, address owner, address creator, address[] signers, uint required, string indentifier);
+    event FactoryBeaconOwnerChanged(address indexed oldOwner, address indexed newOwner);
 
-    constructor(address implementation_, address[] memory signers, uint required, address owner) initializer {
-        _transferOwnership(owner);
+    constructor(address implementation_, address[] memory signers, uint required, address _owner) initializer {
+        _transferOwnership(_owner);
         UpgradeableBeaconMultisig _beacon = new UpgradeableBeaconMultisig(
             implementation_,
             signers,
             required
         );
-        _beacon.transferOwnership(owner);
+        _beacon.transferOwnership(_owner);
         beacon = address(_beacon);
+        owner = _owner;
     }
 
     function create(
@@ -28,9 +30,15 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
         uint required,
         string calldata indentifier
     ) external returns (address) {
-        address proxyAddress = address(new BeaconProxy(beacon, abi.encodeWithSelector(MultiSigWalletAPI(payable(address(0))).initialize.selector, signers, required, _msgSender(), indentifier)));
+        address proxyAddress = address(new BeaconProxy(beacon, abi.encodeWithSelector(MultiSigWalletAPI(payable(address(0))).initialize.selector, signers, required, owner, indentifier)));
         beacons.push(proxyAddress);
-        emit VaultCreated(proxyAddress, _msgSender(), signers, required, indentifier);
+        emit VaultCreated(proxyAddress, owner, _msgSender(), signers, required, indentifier);
         return proxyAddress;
+    }
+
+    function setOwner(address _newOwner) onlyOwner external {
+        address oldOwner = owner;
+        owner = _newOwner;
+        emit FactoryBeaconOwnerChanged(oldOwner, _newOwner);
     }
  }
